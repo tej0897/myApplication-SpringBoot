7077b668a3842111d2f850a730ce1de5d08bb1bc
tej0897 <tej897@gmail.com>
2024-03-24 23:34:27 +0530

    commit-2

--- myApplication/src/main/java/com/learning/myApplication/Rest/RestControllers.java
+++ myApplication/src/main/java/com/learning/myApplication/Rest/RestControllers.java
@@ -1,15 +1,26 @@
 package com.learning.myApplication.Rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestControllers {
 
+    @Value("${user.name}")
+    private String userName;
 
-    @GetMapping("/print")
+    @GetMapping("/")
     public String printHello(){
-        return "Hello World!!";
+        return "Hello World!!" + getUserName();
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 
 }
