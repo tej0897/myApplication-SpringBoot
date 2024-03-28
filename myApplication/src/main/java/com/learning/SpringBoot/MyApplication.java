53ec39feb0511f839eb69586e063a6bfa54b60fb
tej0897 <tej897@gmail.com>
2024-03-28 20:10:54 +0530

    dev tools commit

--- myApplication/src/main/java/com/learning/SpringBoot/MyApplication.java
+++ myApplication/src/main/java/com/learning/SpringBoot/MyApplication.java
@@ -1,4 +1,4 @@
-package com.learning.myApplication;
+package com.learning.SpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- myApplication/src/test/java/com/learning/SpringBoot/MyApplicationTests.java
+++ myApplication/src/test/java/com/learning/SpringBoot/MyApplicationTests.java
@@ -1,4 +1,4 @@
-package com.learning.myApplication;
+package com.learning.SpringBoot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
